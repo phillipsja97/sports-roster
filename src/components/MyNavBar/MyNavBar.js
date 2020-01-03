6ba32a2347d9327b318c32522bf5e5d906ac22be
Jamie <phillipsja97@gmail.com>
2020-01-03 12:18:28 -0600

    styling finished

--- src/components/MyNavBar/MyNavBar.js
+++ src/components/MyNavBar/MyNavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import './MyNavBar.scss';
 
 class MyNavBar extends React.Component {
   static propTypes = {
@@ -16,15 +17,21 @@ class MyNavBar extends React.Component {
   render() {
     const { authed } = this.props;
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <span className="navbar-brand">Charlestown Chiefs</span>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <span className="navbar-brand" href="#">
+          <img className="nav-logo" src="https://vignette.wikia.nocookie.net/hockeymovies/images/d/d0/Chiefs_logo.png/revision/latest/scale-to-width-down/340?cb=20130611235924" alt="Chiefs"/>
+        </span>
+        <span className="navbar-brand nav-text" href="#">Charlestown Chiefs Roster</span>
+        <button className="navbar-toggler" type="button"
+          data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-              { authed && (<button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button>)}
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
           </ul>
+          <div className="form-inline my-2 my-lg-0">
+            { authed && <button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button> }
+          </div>
         </div>
       </nav>
     );
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -29,15 +29,17 @@ class Player extends React.Component {
     const { player } = this.props;
     return (
        <div className="player col-4">
-         <div className="card">
+         <div className="card" id="playerCard">
+           <div className="d-flex justify-content-end closeOut">
+         <button className="btn btn-danger" onClick={this.deleteSinglePlayerEvent}>X</button>
+         </div>
            <div className="card-body">
              <h5 className="card-title text-center">{player.name}</h5>
              <p className="card-text text-center">{player.position}</p>
              <img src={player.imageUrl} alt={player.name} id="playerPhoto"/>
            </div>
            <div className="d-inline-flex justify-content-center" id="cardButtons">
-           <button className="btn btn-outline-warning" onClick={this.setEditModeEvent} id="updatePlayerButton">Update Player Info</button>
-           <button className="btn btn-outline-danger" onClick={this.deleteSinglePlayerEvent} id="deletePlayerButton">Delete Player</button>
+           <button className="btn btn-warning" onClick={this.setEditModeEvent} id="updatePlayerButton">Update Player Info</button>
            </div>
         </div>
       </div>
